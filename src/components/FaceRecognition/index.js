5c2cdfe72a73c55e98eb52eea0b90b20d38b8ef9
amrinaas96 <amrinaas96@gmail.com>
2023-03-04 20:18:38 +0700

    hidden component when image not exist

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -4,8 +4,8 @@ import './faceRecognition.css'
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center'>
-        <div className='mt2 center relative'>
-            <img id='inputImage' alt='face-recognition' className='center flex' src={'https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Hanks_face.jpg'} width='500px' height='auto' />
+        <div className={`mt2 center relative ${imageUrl ? '' : 'dn'}`}>
+            <img id='inputImage' alt='face-recognition' className='center flex' src={imageUrl} width='500px' height='auto' />
             <div className='bounding-box' style={{ top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
         </div>
     </div>
