75025b60c2eaef1a96602e5442fba11432b4815c
amrinaas96 <amrinaas96@gmail.com>
2023-03-04 20:18:16 +0700

    Refactor clarifai function and move to .env.example

--- src/App.js
+++ src/App.js
@@ -6,48 +6,6 @@ import FaceRecognition from './components/FaceRecognition'
 import SignIn from './components/SignIn'
 import Register from './components/Register'
 import ParticlesBg from 'particles-bg'
-
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = process.env.REACT_APP_PAT_CLARIFAI;
-
-// Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
-const USER_ID = 'clarifai';       
-const APP_ID = 'main';
-
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection';
-const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-const IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Hanks_face.jpg';
-
-///////////////////////////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-///////////////////////////////////////////////////////////////////////////////////
-
-const raw = JSON.stringify({
-    "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
-    },
-    "inputs": [
-        {
-            "data": {
-                "image": {
-                    "url": IMAGE_URL
-                }
-            }
-        }
-    ]
-});
-
-const requestOptions = {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
-    },
-    body: raw
-};
  
 export default class App extends Component {
   constructor() {
@@ -68,10 +26,10 @@ export default class App extends Component {
     const height = Number(image.height);
     console.log(image, width, height, clarifyFace);
     return {
-      leftCol: clarifyFace.left_col * 1800,
+      leftCol: clarifyFace.left_col * width,
       topRow: clarifyFace.top_row * height,
-      rightCol: 1800 - (clarifyFace.right_col * 1800),
-      bottomRow: height - (clarifyFace.bottom_row * height)
+      rightCol: clarifyFace.right_col * width,
+      bottomRow: clarifyFace.bottom_row * height
     }
   }
 
@@ -94,7 +52,47 @@ export default class App extends Component {
   }
 
   onSubmit = () => {
-    console.log('Click!');
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = process.env.REACT_APP_PAT_CLARIFAI;
+
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = process.env.REACT_APP_USER_ID_CLARIFAI;       
+    const APP_ID = process.env.REACT_APP_APP_ID_CLARIFAI;
+
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = process.env.REACT_APP_MODEL_ID_CLARIFAI;
+    const MODEL_VERSION_ID = process.env.REACT_APP_MODEL_VERSION_ID_CLARIFAI;    
+    const IMAGE_URL = this.state.input;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
     this.setState({ imageUrl: this.state.input})
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
         .then(response => response.text())
