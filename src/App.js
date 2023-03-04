d1732ecbd8b9858569fc1f7c64c4f545f40b31f9
amrinaas96 <amrinaas96@gmail.com>
2023-03-04 18:18:57 +0700

    Import all component and and functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navigation from './components/Navigation'
+import ImageLinkForm from './components/ImageLinkForm'
+import Rank from './components/Rank'
+import FaceRecognition from './components/FaceRecognition'
+import SignIn from './components/SignIn'
+import Register from './components/Register'
+import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = 'cc52b731959843bda3b38624b749304c';
+
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'clarifai';       
+const APP_ID = 'main';
+
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+const IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Hanks_face.jpg';
+
+///////////////////////////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
+
+const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+ 
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifyFace = JSON.parse(data).outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(image, width, height, clarifyFace);
+    return {
+      leftCol: clarifyFace.left_col * 1800,
+      topRow: clarifyFace.top_row * height,
+      rightCol: 1800 - (clarifyFace.right_col * 1800),
+      bottomRow: height - (clarifyFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log('box', box);
+    this.setState({ box: box })
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignIn: true})
+    }
+    this.setState({route: route})
+  }
+
+  onSubmit = () => {
+    console.log('Click!');
+    this.setState({ imageUrl: this.state.input})
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.text())
+        .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+        .catch(error => console.log('error', error));
+  }
+
+  render() {
+    const {isSignIn, imageUrl, route, box} = this.state;
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
+        <Navigation onRouteChange={this.onRouteChange} isSignIn={isSignIn} />
+        {route === 'home' ? 
+          <div>
+            <Rank />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange} 
+              onSubmit={this.onSubmit} 
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>  
+          : ( route === 'signin' ? 
+              <SignIn onRouteChange={this.onRouteChange} /> : <Register onRouteChange={this.onRouteChange} /> 
+            ) 
+          
+          
+           
+    }
+      </div>
+    );
+  }
 }
-
-export default App;
