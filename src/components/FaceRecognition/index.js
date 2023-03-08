e33fb74f94c9f00d2a64bf8cde608f03db87e6d6
amrinaas96 <amrinaas96@gmail.com>
2023-03-08 22:19:25 +0700

    Fix styling

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -3,9 +3,9 @@ import './faceRecognition.css'
 
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className='center'>
-        <div className={`mt2 center relative ${imageUrl ? '' : 'dn'}`}>
-            <img id='inputImage' alt='face-recognition' className='center flex' src={imageUrl} width='500px' height='auto' />
+    <div className=''>
+        <div className={`mt2  relative ${imageUrl ? '' : 'dn'}`}>
+            <img id='inputImage' alt='face-recognition' className=' flex' src={imageUrl} width='500px' height='auto' />
             <div className='bounding-box' style={{ top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
         </div>
     </div>
