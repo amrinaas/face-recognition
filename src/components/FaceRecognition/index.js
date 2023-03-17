6aec9c24160f5d177b2868772c90ea662e944656
amrinaas96 <amrinaas96@gmail.com>
2023-03-17 11:07:43 +0700

    mapping bounding box and fix styling

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './faceRecognition.css'
 
 const FaceRecognition = ({ imageUrl, box }) => {
+  const listBoundaryBox = box.map((c, d) =>
+      <div className='bounding-box' key={d} style={{ top: c.topRow, right:c.rightCol, bottom: c.bottomRow, left: c.leftCol }}></div>
+    );
   return (
-    <div className=''>
-        <div className={`mt2  relative ${imageUrl ? '' : 'dn'}`}>
-            <img id='inputImage' alt='face-recognition' className=' flex' src={imageUrl} width='500px' height='auto' />
-            <div className='bounding-box' style={{ top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+    <div className='image-detect'>
+        <div className={`center mt2 absolute ${imageUrl ? '' : 'dn'}`}>
+            <img id='inputImage' alt='face-recognition' src={imageUrl} width='500px' height='auto' />
+            {listBoundaryBox}
         </div>
     </div>
   )
