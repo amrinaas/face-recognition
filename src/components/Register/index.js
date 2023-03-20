8d7532a2b01d86e2302505cc6adbac77f779cb47
amrinaas96 <amrinaas96@gmail.com>
2023-03-20 21:49:46 +0700

    Handle failed sign in and register

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,85 +1,113 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
+import LoadingSpinner from '../LoadingSpinner';
+import Modal from '../Modal';
 
-class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            name: ''
-        }
-    }
-    
-    onEmailChange = (event) => {
-        this.setState({ email: event.target.value })
-    }
+const Register = (props) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isShowModal, setIsShowModal] = useState(false);
 
-    onPasswordChange = (event) => {
-        this.setState({ password: event.target.value })
-    }
-
-    onNameChange = (event) => {
-        this.setState({ name: event.target.value })
-    }
-
-    onSubmitRegister = () => {
+    const onSubmitRegister = (data) => {
+        setIsLoading(true);
         fetch(process.env.REACT_APP_API_URL+'/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password,
-                name: this.state.name
+                email: data.email,
+                password: data.password,
+                name: data.name
             })
-        }).then(response => response.json())
+        }).then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error('Something went wrong');
+        })
         .then(user => {
             if (user.id) {
-                this.props.loadUser(user)
-                this.props.onRouteChange('home')
+                setIsLoading(false)
+                props.loadUser(user)
+                props.onRouteChange('home')
             }
         })
-        .catch(console.log())
+        .catch(err => {
+            setIsLoading(false)
+            setIsShowModal(true)
+            console.log('error register', err);
+        })
     }
 
-    render() {
-        return (
-          <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-              <main className="pa4 black-80 tc">
-                  <div className="measure">
-                      <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                      <legend className="f1 fw6 ph0 mh0">Register</legend>
-                      <div className="mt3">
-                          <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                          <input 
-                            onChange={this.onNameChange}
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" name="name"  id="name" />
-                      </div>
-                      <div className="mt3">
-                          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                          <input
-                          onChange={this.onEmailChange} 
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" name="email-address"  id="email-address" />
-                      </div>
-                      <div className="mv3">
-                          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                          <input 
-                            onChange={this.onPasswordChange}
-                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="password" name="password"  id="password" />
-                      </div>
-                      </fieldset>
-                      <div className="">
-                          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                              onClick={this.onSubmitRegister}
-                              type="submit" value="Register" />
-                      </div>
-                  </div>
-              </main>
-          </article>
-        )
+    const handleCloseModal = () => {
+        setIsShowModal(false)
     }
+
+    return (
+        <>
+            {isLoading ? <LoadingSpinner /> : 
+                <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                { isShowModal ? <Modal handleClose={handleCloseModal}>
+                    <span>Please contact the developer</span>
+                </Modal> : null }
+                    <main className="pa4 black-80 tc">
+                        <div className="measure">
+                            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Register</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                                <input 
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="text" name="name" id="name" 
+                                    {...register('name', { required: true })
+                                    }
+                                />
+                                {errors.name && (
+                                    <small className='red' role="alert">
+                                    This field is required
+                                    </small>
+                                )}
+                            </div>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="email" name="email-address"  id="email-address" 
+                                    {...register('email', { required: true, pattern: {
+                                        value: /\S+@\S+\.\S+/,
+                                        message: "Entered value does not match email format" }})
+                                    }
+                                />
+                                {errors.email && (
+                                    <small className='red' role="alert">
+                                    {errors.email.message}
+                                    </small>
+                                )}
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input 
+                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="password" name="password"  id="password" 
+                                    {...register('password', { required: true })}
+                                />
+                                {errors.password && (
+                                <small className='red' role="alert">
+                                This field is required
+                                </small>
+                            )}
+                            </div>
+                            </fieldset>
+                            <div className="">
+                                <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                    onClick={handleSubmit(onSubmitRegister)}
+                                    type="submit" value="Register" />
+                            </div>
+                        </div>
+                    </main>
+                </article>
+            }
+        </>
+    )
 }
 
 export default Register
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,76 +1,103 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
+import LoadingSpinner from '../LoadingSpinner';
+import Modal from '../Modal';
 
-class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            signInEmail: '',
-            signInPassword: ''
-        }
-    }
-    
-    onEmailChange = (event) => {
-        this.setState({ signInEmail: event.target.value })
-    }
+const SignIn = (props) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isShowModal, setIsShowModal] = useState(false);
 
-    onPasswordChange = (event) => {
-        this.setState({ signInPassword: event.target.value })
-    }
-
-    onSubmitSignIn = () => {
+    const onSubmitSignIn = (data) => {
+        setIsLoading(true);
         fetch(process.env.REACT_APP_API_URL+'/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                email: this.state.signInEmail,
-                password: this.state.signInPassword
+                email: data.email,
+                password: data.password
             })
-        }).then(response => response.json())
-        .then(user => {
-            if (user.id) {
-                this.props.loadUser(user)
-                this.props.onRouteChange('home')
-            }
         })
-        .catch(console.log())
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error('Something went wrong');
+        })
+        .then(
+            (user) => {
+            if (user.id) {
+                setIsLoading(false)
+                props.loadUser(user)
+                props.onRouteChange('home')
+                }
+            })
+        .catch(err => {
+            setIsLoading(false)
+            setIsShowModal(true)
+            console.log('error sign in', err);
+        })   
     }
 
-    render() {
-        const { onRouteChange } = this.props;
-        return (
-          <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-              <main className="pa4 black-80 tc">
-                  <div className="measure">
-                      <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                      <div className="mt3">
-                          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                          <input
-                            onChange={this.onEmailChange}
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" name="email-address"  id="email-address" />
-                      </div>
-                      <div className="mv3">
-                          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                          <input 
-                            onChange={this.onPasswordChange}
-                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="password" name="password"  id="password" />
-                      </div>
-                      </fieldset>
-                      <div className="">
-                          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                              onClick={this.onSubmitSignIn}
-                              type="submit" value="Sign in" />
-                      </div>
-                      <div className="lh-copy mt3">
-                      <p href="#0" onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-                      </div>
-                  </div>
-              </main>
-          </article>
-        )
+    const handleCloseModal = () => {
+        setIsShowModal(false)
     }
+
+  return (
+    <>
+        {isLoading ? <LoadingSpinner /> : 
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                { isShowModal ? <Modal handleClose={handleCloseModal}>
+                    <span>Please check your email and password</span>
+                </Modal> : null }
+                <main className="pa4 black-80 tc">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input
+                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" name="email-address"  id="email-address"
+                            {...register('email', { required: true, pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Entered value does not match email format"}})
+                            }
+                            />
+                            {errors.email && (
+                                <small className='red' role="alert">
+                                {errors.email.message}
+                                </small>
+                            )}
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input 
+                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="password" name="password"  id="password"
+                            {...register('password', { required: true })}
+                            />
+                            {errors.password && (
+                                <small className='red' role="alert">
+                                This field is required
+                                </small>
+                            )}
+                        </div>
+                        </fieldset>
+                        <div className="">
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                onClick={handleSubmit(onSubmitSignIn)} disabled={isLoading}
+                                type="submit" value="Sign in" />
+                        </div>
+                        <div className="lh-copy mt3">
+                        <p href="#0" onClick={() => props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                        </div>
+                    </div>
+                </main>
+            </article>
+        }
+    </>
+  )
 }
 
 export default SignIn
