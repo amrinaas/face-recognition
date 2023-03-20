fa984e9bfe16502625c19552373620889e5cf0c1
amrinaas96 <amrinaas96@gmail.com>
2023-03-20 21:49:28 +0700

    Add spinner component

---
+++ src/components/LoadingSpinner/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./spinner.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="loading">
+      <div className="loader">
+      </div>
+    </div>
+  );
+}
